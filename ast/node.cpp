a7600776adbdb522fd660655a9815a8ecb9cb19b
Kasen <CHNZhukc@outlook.com>
2021-06-14 16:12:19 +0800

    opt code

--- ast/node.cpp
+++ ast/node.cpp
@@ -2,8 +2,8 @@
 
 Node::Node() // Bison needs this
 {
-    type = "uninitialised";
-    value = "uninitialised";
+    this->type = "uninitialised";
+    this->value = "uninitialised";
 }
 
 Node::Node(string t, string v) : type(t), value(v) {}
@@ -13,7 +13,7 @@ void Node::printAST(int depth)
     for (int i = 0; i < depth; ++i)
         cout << "  ";
     cout << this->type << ":" << this->value << endl;
-    for (auto i = children.begin(); i != children.end(); ++i)
+    for (auto i = this->children.begin(); i != this->children.end(); ++i)
         (*i)->printAST(depth + 1);
 }
 
@@ -22,19 +22,19 @@ void Node::saveAST(std::ofstream *outStream, int depth)
     for (int i = 0; i < depth; ++i)
         *outStream << "  ";
     *outStream << this->type << ":" << this->value << endl;
-    for (auto i = children.begin(); i != children.end(); ++i)
+    for (auto i = this->children.begin(); i != this->children.end(); ++i)
         (*i)->saveAST(outStream, depth + 1);
 }
 
 void Node::generateAST(int &count, std::ofstream *outStream)
 {
     this->id = ++count;
-    if (value != "")
+    if (this->value != "")
         *outStream << "n" << this->id << " [label=\"" << this->type << ":" << this->value << "\"];" << endl;
     else
         *outStream << "n" << this->id << " [label=\"" << this->type << "\"];" << endl;
 
-    for (auto i = children.begin(); i != children.end(); ++i)
+    for (auto i = this->children.begin(); i != this->children.end(); ++i)
     {
         (*i)->generateAST(count, outStream);
         *outStream << "n" << this->id << " -> n" << (*i)->id << endl;
