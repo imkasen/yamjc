3bf5d8e96f2fce6bd4e286a6fa8685aa9495199e
Kasen <CHNZhukc@outlook.com>
2021-07-31 17:18:06 +0800

    finish st:FormalParameterList

--- ast/formal_parameter_list.cpp
+++ ast/formal_parameter_list.cpp
@@ -1,5 +1,40 @@
 #include "formal_parameter_list.h"
+using std::string;
+using std::size_t;
 
 FormalParameterList::FormalParameterList() : Node() {}
 
-FormalParameterList::FormalParameterList(std::string t, std::string v) : Node(t, v) {}
+FormalParameterList::FormalParameterList(string t, string v) : Node(t, v) {}
+
+/*
+ * 1.
+ * Create records in the current "Method" scope.
+ * Scope add:
+ *     Variable <name>
+ *
+ * 2.
+ * Get the "Method" record in the "Class" scope.
+ * Add methods into "Method".
+ *
+ * @return: std::nullopt
+ */
+std::optional<string> FormalParameterList::generateST()
+{
+    for (size_t i = 0; i < this->children.size(); i += 2)
+    {
+        // create parameter records
+        string parameter_name, parameter_type;
+        parameter_type = this->children.at(i)->generateST().value_or("Unknown");
+        parameter_name = this->children.at(i + 1)->generateST().value_or("Unknown");
+        std::shared_ptr<Variable> parameter_ptr = std::make_shared<Variable>(parameter_name, parameter_type);
+        FormalParameterList::st.addRecord(parameter_name, parameter_ptr);
+
+        // add parameter records into "Method"
+        string scope_name = FormalParameterList::st.getScopeTitle();
+        auto record_ptr = FormalParameterList::st.lookupRecord(scope_name).value_or(nullptr);
+        auto method_ptr = std::dynamic_pointer_cast<Method>(record_ptr);
+        method_ptr->addParameter(parameter_ptr);
+    }
+
+    return std::nullopt;
+}
--- ast/formal_parameter_list.h
+++ ast/formal_parameter_list.h
@@ -8,6 +8,8 @@ class FormalParameterList : public Node
 public:
     FormalParameterList();
     FormalParameterList(std::string t, std::string v);
+
+    std::optional<std::string> generateST() override;
 };
 
 #endif
