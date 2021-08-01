000f616b0779959c854d63f79289768e4c10e0f0
Kasen <CHNZhukc@outlook.com>
2021-08-01 16:15:03 +0800

    update st:Type

--- ast/type.cpp
+++ ast/type.cpp
@@ -6,5 +6,14 @@ Type::Type(string t, string v) : Node(t, v) {}
 
 std::optional<string> Type::generateST()
 {
-    return this->getValue();
+    if (!this->getValue().empty() && this->children.size() == 0) // has value && no children
+    {
+        return this->getValue();
+    }
+    else if (this->getValue().empty() && this->children.size() == 1) // no value && has one "Identifier" child
+    {
+        return this->children.at(0)->generateST().value_or("Unknown");
+    }
+
+    return std::nullopt;
 }
