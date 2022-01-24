8db2833d53e1db26e4c7481088439ff48691d4e9
Kasen <CHNZhukc@outlook.com>
2022-01-24 21:13:38 +0800

    simplify graph.sh

--- graph.sh
+++ graph.sh
@@ -2,18 +2,10 @@
 
 set -Eeuo pipefail
 
-ast() {
-  dot -T svg ast.dot -o ast.svg
-}
-
-st() {
-  dot -T svg st.dot -o st.svg
-}
-
 if [[ -e ast.dot ]]; then
-  ast
+  dot -T svg ast.dot -o ast.svg
 fi
 
 if [[ -e st.dot ]]; then
-  st
+  dot -T svg st.dot -o st.svg
 fi
